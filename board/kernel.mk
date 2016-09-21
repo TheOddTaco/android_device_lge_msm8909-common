5fd71c7367b7d553bd2f18fe086efb8a73ef2b70
Aleksey Klinov <qwertyTom@users.noreply.github.com>
2016-09-21 17:21:36 +0300

    Update kernel.mk

--- board/kernel.mk
+++ board/kernel.mk
@@ -1,4 +1,4 @@
-BOARD_DTBTOOL_ARGS := -2
+BOARD_DTBTOOL_ARGS := -3
 
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.selinux=permissive androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1
