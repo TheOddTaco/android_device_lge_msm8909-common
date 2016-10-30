954af67fa8f191dc12b8425ee41ce2fd54c0e481
Taco <derpytacozlol@gmail.com>
2016-10-30 14:57:38 -0400

    Update 00-qcom-platform-msm8909.mk

--- board/00-qcom-platform-msm8909.mk
+++ board/00-qcom-platform-msm8909.mk
@@ -7,7 +7,7 @@ TARGET_ARCH := arm
 TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
-TARGET_CPU_VARIANT := cortex-a53
+TARGET_CPU_VARIANT := cortex-a7
 
 # Init
 TARGET_PLATFORM_DEVICE_BASE := /devices/soc.0/
